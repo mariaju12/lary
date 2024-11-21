ba6d91c9ef93e51ee12cbe908e89ca975c36f9ec
mariaju12 <00001121349808sp@al.educacao.sp.gov.br>
2024-11-21 14:05:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Disney',
+    'Cite 5 princesas',
+    '1- Rapunzel, 2- Tiana, 3- Cinderela, 4-Branca de neve, 5-Bela e a fera'
+)
+
+criaCartao(
+    'Disney',
+    'Quem criou a Disney?',
+    'Walt Disney'
+)
+
+criaCartao(
+    'Disney',
+    'Qual a pincesa mais famosa?',
+    'Cinderela'
+)
+
+criaCartao(
+    'Disney',
+    'Qual é a princesa mais velha?',
+    'Branca de neve'
+)
